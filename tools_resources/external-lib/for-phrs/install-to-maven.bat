83439a8541426ccf0c7d012c2378f13c52d67239
M0puffu5 <M0puffu5@gmail.com>
2011-12-19 16:05:00 +0000

    I use the "call" statement for the maven calls. The script is now correct according with the ms-bash specs.

--- tools_resources/external-lib/for-phrs/install-to-maven.bat
+++ tools_resources/external-lib/for-phrs/install-to-maven.bat
@@ -2,9 +2,9 @@ cls
 
 echo on
 
-mvn install:install-file  -Dfile=QUPC_AR004040UV_Service.jar  -Dsource=QUPC_AR004040UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004040UV_Service -Dversion=0.1 -Dpackaging=jar
+call mvn install:install-file  -Dfile=QUPC_AR004040UV_Service.jar  -Dsource=QUPC_AR004040UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004040UV_Service -Dversion=0.1 -Dpackaging=jar
 echo "QUPC_AR004040UV_Service was install"
 
-mvn install:install-file  -Dfile=QUPC_AR004030UV_Service.jar  -Dsource=QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar
+call mvn install:install-file  -Dfile=QUPC_AR004030UV_Service.jar  -Dsource=QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar
 
 echo "QUPC_AR00400UV_Service was install"
