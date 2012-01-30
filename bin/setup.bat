12e08d2706311397b79d291d33eeb9b0a2090654
M0puffu5 <M0puffu5@gmail.com>
2012-01-30 15:22:43 +0000

    the phrweb scripting is away, the install is done via mvn install

--- bin/setup.bat
+++ bin/setup.bat
@@ -67,6 +67,3 @@ echo QUPC_AR004040UV_Service (binary and sources) was install
 call mvn install:install-file  -Dfile=..\tools_resources\external-lib\for-phrs\QUPC_AR004030UV_Service.jar  -Dsource=..\tools_resources\external-lib\for-phrs\QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar
 echo QUPC_AR004030UV_Service (binary and sources) was install
 
-call ..\icardea-phrs\phrweb\package.bat
-echo The phrweb was installed.
-
