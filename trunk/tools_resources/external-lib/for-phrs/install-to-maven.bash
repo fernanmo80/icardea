5a50d8462adfb38440288f82610ecf46efb8125d
fstroh@gmail.com <fstroh@gmail.com@b378eec8-abba-c587-db4e-a5ee84d730a4>
2012-01-31 14:45:07 +0000

    pom-file has not been generated in one of our installations. Adding the generatePom=true parameter fixed that minor problem of the installation process.

--- trunk/tools_resources/external-lib/for-phrs/install-to-maven.bash
+++ trunk/tools_resources/external-lib/for-phrs/install-to-maven.bash
@@ -2,7 +2,7 @@
 #set -xv
 
 
-mvn install:install-file  -Dfile=QUPC_AR004040UV_Service.jar  -Dsource=QUPC_AR004040UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004040UV_Service -Dversion=0.1 -Dpackaging=jar
+mvn install:install-file  -Dfile=QUPC_AR004040UV_Service.jar  -Dsource=QUPC_AR004040UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004040UV_Service -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
 
-mvn install:install-file  -Dfile=QUPC_AR004030UV_Service.jar  -Dsource=QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar
+mvn install:install-file  -Dfile=QUPC_AR004030UV_Service.jar  -Dsource=QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
 
