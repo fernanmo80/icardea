e0d5add618cdaad2e6336455ea5d23e301afa13a
thiel.offis <thiel.offis@gmail.com>
2012-02-03 11:23:22 +0000

    correct path

--- bin/runmongodb.bat
+++ bin/runmongodb.bat
@@ -1,2 +1,2 @@
 call title MONGO DB
-call ..\tools\mongo\mongodb-win32-i386-1.8.0\bin\mongod --dbpath ..\data --directoryperdb
+call ..\tools\mongo\mongodb-win32-i386-1.8.3\bin\mongod --dbpath ..\data --directoryperdb
