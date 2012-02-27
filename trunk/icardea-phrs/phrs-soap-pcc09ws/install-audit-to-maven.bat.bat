9cd042dd4b78d4e5e7f1f88b6b382e2e9906e86d
bmulreni@gmail.com <bmulreni@gmail.com@b378eec8-abba-c587-db4e-a5ee84d730a4>
2012-02-27 14:49:09 +0000

    restore maven files to install audit jar

---
+++ trunk/icardea-phrs/phrs-soap-pcc09ws/install-audit-to-maven.bat.bat
@@ -0,0 +1,8 @@
+cls
+
+echo on
+ 
+call mvn install:install-file   -Dfile=icardea-atnalog-client-1.0-SNAPSHOT.jar -DgroupId=tr.com.srdc.icardea -DartifactId=icardea-atnalog-client -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
+echo "Audit icardea-atnalog-client  was install"
+
+pause
