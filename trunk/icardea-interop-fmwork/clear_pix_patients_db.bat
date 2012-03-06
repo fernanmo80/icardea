8a89aa86221b7d61a36f296ebdd718f8ead97172
sgsfak@gmail.com <sgsfak@gmail.com@b378eec8-abba-c587-db4e-a5ee84d730a4>
2012-03-06 14:28:30 +0000

    scripts for clearing databases

---
+++ trunk/icardea-interop-fmwork/clear_pix_patients_db.bat
@@ -0,0 +1,8 @@
+@echo off
+if NOT DEFINED MONGO_HOME set MONGO_HOME=D:\local\mongodb-win32-i386-1.6.3
+for /f "delims=" %%a in ('%MONGO_HOME%\bin\mongo localhost/pid -quiet -eval "db.patients.count()"') do @set delcount=%%a
+echo You are going to delete %delcount% patient records in the PIX database.
+echo Are you sure? (press Ctrl-C otherwise)
+pause
+%MONGO_HOME%\bin\mongo localhost/pid -quiet -eval "db.patients.remove()"
+echo Done!
---
+++ trunk/icardea-interop-fmwork/clear_ub_docs_db.bat
@@ -0,0 +1,8 @@
+@echo off
+if NOT DEFINED MONGO_HOME set MONGO_HOME=D:\local\mongodb-win32-i386-1.6.3
+for /f "delims=" %%a in ('%MONGO_HOME%\bin\mongo localhost/xds -quiet -eval "db.pcc.count()"') do @set delcount=%%a
+echo You are going to delete %delcount% subscriptions in the Update Broker db.
+echo Are you sure? (press Ctrl-C otherwise)
+pause
+%MONGO_HOME%\bin\mongo localhost/xds -quiet -eval "db.pcc.remove()"
+echo Done! Don't forget to restart the Update Broker if it's running now..
