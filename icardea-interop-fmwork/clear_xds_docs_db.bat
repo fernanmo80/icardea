87f6715c07a201ca5d5293a8406e689716d2cec6
sgsfak <sgsfak@gmail.com>
2012-03-06 14:28:30 +0000

    scripts for clearing databases

---
+++ icardea-interop-fmwork/clear_xds_docs_db.bat
@@ -0,0 +1,8 @@
+@echo off
+if NOT DEFINED MONGO_HOME set MONGO_HOME=D:\local\mongodb-win32-i386-1.6.3
+for /f "delims=" %%a in ('%MONGO_HOME%\bin\mongo localhost/xds -quiet -eval "db.docs.count()"') do @set delcount=%%a
+echo You are going to delete %delcount% documents in the XDS database.
+echo Are you sure? (press Ctrl-C otherwise)
+pause
+%MONGO_HOME%\bin\mongo localhost/xds -quiet -eval "db.docs.remove()"
+echo Done!
